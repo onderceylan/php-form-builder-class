eb1a04dc509a48cfd0cde890c688fb23063a8e52
Önder Ceylan <onder@vegno.com>
2013-03-08 17:52:46 +0200

    forked main PFBC project to make it possible to select multiple values on Select element which has option "multiple". Radio element is also updated for similar purposes. (Passing multiple values - in array)

--- PFBC/Element/Select.php
+++ PFBC/Element/Select.php
@@ -19,10 +19,12 @@ class Select extends \PFBC\OptionElement {
 		$selected = false;
 		foreach($this->options as $value => $text) {
 			$value = $this->getOptionValue($value);
+            //var_dump($this->_attributes["value"]);
 			echo '<option value="', $this->filter($value), '"';
 			if(!$selected && in_array($value, $this->_attributes["value"])) {
 				echo ' selected="selected"';
-				$selected = true;
+				if(empty($this->_attributes["multiple"]))
+                    $selected = true;
 			}	
 			echo '>', $text, '</option>';
 		}	
